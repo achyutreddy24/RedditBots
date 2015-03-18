ada71888e5a7fe0e68a5833b65aefddbb2f66d23
Achyut Reddy <achyutreddy24@gmail.com>
2015-03-17 23:08:19 -0400

    UTF-8 encoding for txt file

--- AsciiBot/main.py
+++ AsciiBot/main.py
@@ -68,12 +68,9 @@ def scan():
             cur.execute('SELECT * FROM posts WHERE CID=?', [cid])
             if not cur.fetchone():
                 print("Found an ascii comment")
-                try:
-                    f = open('CurrentAscii.txt', 'w')
-                    f.write(cfullBody)
-                    f.close()
-                except Exception as e:
-                    print(cfullBody)
+                f = open('CurrentAscii.txt', 'w', encoding='utf-8')
+                f.write(cfullBody)
+                f.close()
                 
                 mp.make_jpg('CurrentAscii', 'CurrentJPG')
                 ILink = upload_imgur('CurrentJPG.jpg')['link']
