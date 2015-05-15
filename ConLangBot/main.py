8a7e45c4a025f367e1414ec1507b053ec51124c0
Achyut Reddy <achyutreddy24@gmail.com>
2015-05-15 19:23:37 -0400

    Almost done i think

--- ConLangBot/main.py
+++ ConLangBot/main.py
@@ -1,8 +1,9 @@
 import praw # simple interface to the reddit API, also handles rate limiting of requests
 import time
+import sqlite3
 import re
 
-SUMMONTEXT = """\+\/u\/conlangbot\s*\"([\s\S]+)\""""
+SUMMONTEXT = """\+\/u\/fusiongaming\s*\"([\s\S]+)\""""
 
 #  Import Settings from Config.py
 try:
@@ -12,7 +13,7 @@ try:
     USERAGENT = Config.USERAGENT
     MAXPOSTS = Config.MAXPOSTS
     REPLYMESSAGE = Config.REPLYMESSAGE
-    SUBREDDIT = COnfig.SUBREDDIT    
+    SUBREDDIT = Config.SUBREDDIT    
 
     print("Loaded Config")
 except ImportError:
@@ -25,32 +26,34 @@ r.login(USERNAME, PASSWORD)
 
 sql = sqlite3.connect('data.db')
 cur = sql.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS posts(CID TEXT, CLink TEXT, CLinkParent TEXT, ILink TEXT)')
+cur.execute('CREATE TABLE IF NOT EXISTS posts(CID TEXT, Word TEXT, SQPOST TEXT)')
 
 cur.execute('CREATE TABLE IF NOT EXISTS threads(PID TEXT, Title TEXT)')
 sql.commit()
 
 print('Loaded SQL Database')
 
-def refresh_db(flairs):
+def refresh_db():
     subreddit = r.get_subreddit(SUBREDDIT)
-	posts = subreddit.get_new(limit=MAXPOSTS)
-	for post in posts:
-		# Anything that needs to happen every loop goes here.
-		pid = post.id
-        cur.execute('SELECT * FROM threads WHERE ID=?', [pid])
-		if cur.fetchone():
+    posts = subreddit.get_new(limit=MAXPOSTS)
+    for post in posts:
+        # Anything that needs to happen every loop goes here.
+        pid = post.id
+        print("Found "+pid+" "+ post.title)
+        cur.execute('SELECT * FROM threads WHERE PID=?', [pid])
+        if cur.fetchone():
             continue
-        if 'Small Questions' in post.title or 'WWSQ' in post.title
-        cur.execute('INSERT INTO threads VALUES(?, ?)', [pid, post.title])
-        sql.commit()
+        if 'Small Questions' in post.title or 'WWSQ' in post.title:
+            cur.execute('INSERT INTO threads VALUES(?, ?)', [pid, post.title])
+            sql.commit()
 
 
 def find_in_submissions(search_string):
-	cur.execute('SELECT * FROM threads')
+    cur.execute('SELECT * FROM threads')
     for row in cur:
-        ID = row(0)
-        post = r.get_info(thing_id=ID)
+        print(row)
+        ID = row[0]
+        post = r.get_submission(submission_id=ID)
         for comment in post.comments:
             if search_string in comment.body.lower():
                 if comment.is_root:
@@ -61,32 +64,47 @@ def find_in_submissions(search_string):
 def scan():
     refresh_db()
 
-    comments = r.get_mentions(limit=MAXPOSTS)
+    comments = r.get_mentions(limit=5)
     for comment in comments:
         
         cbody = comment.body.lower()
-        Clink = comment.permalink
         cid = comment.id
         
+        print(cid)
+        print(cbody)
 
         match = re.search(SUMMONTEXT, cbody)
         if not match:
+            print("Comment has no summon text")
             continue
+            
         word = match.group(1)        
         cur.execute('SELECT * FROM posts WHERE CID=?', [cid])
         if not cur.fetchone():
             print("Found a summon comment")
             
-            def_post = find_in_submission(word)
+            def_post = find_in_submissions(word)
+            if def_post:
+                author = def_post.author
+                body = def_post.body
+                link = def_post.short_link
+                iden = def_post.id
+                print('Replying to ' + cid)
+                comment.reply(REPLYMESSAGE.format(text=body, author=author, link=link))
+            else:
+                print('Replying to ' + cid)
+                comment.reply("Sorry could not find that comment in any small questions thread.")
+                iden = 'NOT FOUND'
 
-            author = def_post.author
-            body = def_post.body
-            link = def_post.short_link
-            
-            print('Replying to ' + cid)
-            comment.reply(REPLYMESSAGE.format(ILink))
-            
-            cur.execute('INSERT INTO posts VALUES(?, ?, ?, ?)', [cid, Clink, ClinkParent, ILink])
+            cur.execute('INSERT INTO posts VALUES(?, ?, ?)', [cid, word, iden])
             sql.commit()
         else:
             print("Already replied to that comment")
+
+while True:
+    #try:
+    scan()
+    #except Exception as e:
+    #    print("ERR", e)
+    print('Sleeping ' + str(WAIT))
+    time.sleep(WAIT)
