af6711c5d80bfd4c602785ec652bacc531928bf7
Achyut Reddy <achyutreddy24@gmail.com>
2015-06-04 16:13:54 -0400

    Updates

--- TwitchToYoutube/RunThis.py
+++ TwitchToYoutube/RunThis.py
@@ -4,7 +4,7 @@ import subprocess
 WAIT = 30
 while True:
     try:
-        subprocess.call("python main.py", shell=True)
+        subprocess.call("python3 main.py", shell=True)
     except Exception as e:
         print("ERROR:", str(e))
     print('Sleeping ' + str(WAIT) + ' seconds.\n')
