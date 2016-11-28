c7abc7d64509d3743de2b29a58c6f0ee5f7d362a
Achyut Reddy <achyutreddy24@gmail.com>
2016-11-27 23:21:15 -0500

    More effective detection

--- TimeStamp/main.py
+++ TimeStamp/main.py
@@ -6,14 +6,17 @@ import OAuth2Util
 
 URL_DETECT = re.compile("""(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.)?youtube\.com\/watch(?:\.php)?\?.*v=)([a-zA-Z0-9\-_]+)((?:\?t=\d+)?)""")
 
-
-
-TIMESTAMP_DETECT = re.compile("""(?:(\d{0,2}):)?(\d{1,2}):(\d{1,2})""")
+TIMESTAMP_DETECT = re.compile("""(?:(\d{0,2}):)?(\d{0,2}):(\d{2})""")
 #TIMESTAMP_DETECT = re.compile("""(\d{1,2}):(\d{2})""")
 
 CON_URL = """https://youtu.be/{vid_id}?t={t}"""
 
-BLOCKED = ['youtubefactsbot']
+BLOCKED = ['youtubefactsbot', 'DeepIntoYouTubeStats', 'Mentioned_Videos']
+KEYWORDS = ['skip to', 'skip', 'timestamp', 'time', 'go to', 'go', 'start', 'at ', 'quote', 'begin', 'found', 'into', 'make']
+
+BLOCKED_KEYWORDS = ['duration', 'a.m', 'p.m', 'est', 'pst', 'gmt', 'pcpart']
+
+BLOCKED_SUBS = ['t5_37jp8']
 
 #  Import Settings from Config.py
 try:
@@ -32,8 +35,6 @@ WAIT = 5
 r = praw.Reddit(USERAGENT)
 #r.login(USERNAME, PASSWORD)
 o = OAuth2Util.OAuth2Util(r)
-
-
 
 sql = sqlite3.connect('comments.db')
 cur = sql.cursor()
@@ -56,24 +57,57 @@ def scan():
         if 'youtu' not in cbody:
             continue
         
-        print(cbody)
+        if ':' not in cbody:
+            continue
+        
+        print(comment.permalink)
+        
+        
+        
+        f=False
+        for word in KEYWORDS:
+            if word in cbody.lower():
+                f = True;
+                break
+        
+        if f==False:
+            continue
+       
+        f=False
+        for word in BLOCKED_KEYWORDS:
+            if word in cbody.lower():
+                f = True;
+                break
+        
+        if f==True:
+            continue 
+        
+        #print(cbody)
         
         Clink = comment.permalink
         cid = comment.id
         author = comment.author
         
         if author.name in BLOCKED:
+            print('blocked user')
+            continue
+            
+        if comment.subreddit_id in BLOCKED_SUBS:
+            print('blocked sub')
             continue
         
         match_yt = URL_DETECT.search(cbody)
         if match_yt is None:
+            print('no match')
             continue
             
         if match_yt.group(2) is not '': # TimeStamp
+            print('timestampt exists')
             continue
                 
         match_ts = TIMESTAMP_DETECT.search(cbody)
         if match_ts is None:
+            print('no timestamp in text')
             continue
 
         cur.execute('SELECT * FROM posts WHERE CID=?', [cid])
@@ -83,7 +117,7 @@ def scan():
             yt_id = match_yt.group(1) # Video ID
             
             hours = int(match_ts.group(1)) if match_ts.group(1) is not None else 0
-            minutes = int(match_ts.group(2))
+            minutes = int(match_ts.group(2)) if match_ts.group(2) is not None else 0
             seconds = int(match_ts.group(3))
             
             total_time = calc_seconds(hours, minutes, seconds)
